78540466366805e77b71245dda72d127939f2c9d
dany <151317720+lillytundra@users.noreply.github.com>
2025-05-02 20:16:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// list of kanade images
+const images = [
+    'images/kanade1.jpg',   
+    'images/kanade2.jpg',
+    'images/kanade3.jpg',
+    'images/kanade4.jpg',
+    'images/kanade5.jpg',
+    'images/kanade6.jpg',
+    'images/kanade7.jpg',
+];
+
+let current = 0;
+const body = document.body;
+const music = document.getElementById('bg-music');
+
+// set initial background
+body.styleBackgroundImage = 'url('${images[current]}')';
+
+// on click, change image and start music
+body.addEventListener('click', () => {
+    current = (current + 1) % images.length;
+    body.style.backgroundImage = 'url('${images[current]}')';
+
+    if (music.paused) {
+        music.play():
+    }
+});
