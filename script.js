6bcd6f80d73226ee984517b231345419adb3d0b7
dany <151317720+lillytundra@users.noreply.github.com>
2025-05-02 21:56:49 +0200

    Update script.js
    go full screen + play music, then change images on click

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// list of kanade images 
+// list of kanade images
 const images = [
     'images/kanade1.jpg',   
     'images/kanade2.jpg',
@@ -22,12 +22,52 @@ const music = document.getElementById('bg-music');
 // set initial background
 body.style.backgroundImage = `url('${images[current]}')`;
 
-// on click, change image and start music
-body.addEventListener('click', () => {
-    current = (current + 1) % images.length;
+// function to update background and play music
+function updateBackground() {
     body.style.backgroundImage = `url('${images[current]}')`;
+}
 
-    if (music.paused) {
-        music.play();
+// flag to check if the first click has been made
+let firstClick = false;
+
+// request fullscreen and play music on the first click
+function goFullscreenAndPlayMusic() {
+    const elem = document.documentElement;
+    
+    // request fullscreen
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { // Safari
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { // IE11
+        elem.msRequestFullscreen();
     }
+
+    // start music
+    music.play();
+
+    firstClick = true;
+    document.removeEventListener('click', goFullscreenAndPlayMusic); // Only do this once
+}
+
+// first click: trigger fullscreen + play music, subsequent clicks: change image
+body.addEventListener('click', () => {
+    if (!firstClick) {
+        goFullscreenAndPlayMusic(); // first click: fullscreen + music
+    } else {
+        current = (current + 1) % images.length;
+        updateBackground(); // subsequent clicks: change image
+    }
+});
+
+// arrow keys
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowRight') {
+        current = (current + 1) % images.length;
+    } else if (event.key === 'ArrowLeft') {
+        current = (current - 1 + images.length) % images.length;
+    } else {
+        return; // exit if not arrow key
+    }
+    updateBackground(); // update background with new image
 });
